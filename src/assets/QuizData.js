c79cdeebfa3885484b80d9edc7499a5b08e53982
RamX23 <112005895+RamX23@users.noreply.github.com>
2024-05-21 07:56:06 +0530

    first commit

---
+++ src/assets/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "Which company developed the TypeScript language?",
+        options: ["Google", "Microsoft", "Facebook", "Apple"],
+        answer: 2
+    },
+    {
+        question: "What does JSON stand for?",
+        options: ["JavaScript Object Notation", "Java Standard Output Network", "JavaScript Output Name", "JavaScript Object Network"],
+        answer: 1
+    },
+    {
+        question: "Which HTML attribute is used to define inline styles?",
+        options: ["class", "style", "font", "styles"],
+        answer: 2
+    },
+    {
+        question: "Which protocol is used to send email?",
+        options: ["FTP", "HTTP", "SMTP", "SSH"],
+        answer: 3
+    },
+    {
+        question: "What does the acronym DOM stand for in web development?",
+        options: ["Document Object Model", "Data Object Model", "Document Observation Model", "Data Object Management"],
+        answer: 1
+    }
+];
